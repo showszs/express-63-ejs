772fa021d584cc2c4ae4c049cda66cf6df101ee1
showszs <pasativan152@gmail.com>
2025-03-22 23:04:04 +0300

    ejs-3

--- src/controllers/articles.mjs
+++ src/controllers/articles.mjs
@@ -1,14 +1,39 @@
-export const getArticleHandler  = (req, res) => {
-    res.end("Get articles Route")
+export const getArticleHandler = (req, res) => {
+    const title = "Articles";
+
+    const articles = [
+        { id: 1, title: 'First Article', content: 'This is the content of the first article.' },
+        { id: 2, title: 'Second Article', content: 'This is the content of the second article.' },
+        { id: 3, title: 'Third Article', content: 'This is the content of the third article.' },
+        { id: 4, title: 'Fourth Article', content: 'This is the content of the fourth article.' },
+        { id: 5, title: 'Fifth Article', content: 'This is the content of the fifth article.' }
+    ];
+
+    res.render('articles', { articles, title });
 }
+
 
 export const postArticleHandler = (req, res) => {
     res.end("Post articles Route")
 }
 
 export const getArticleByIdHandler = (req, res) => {
-    const {articleId} = req.params
-    res.end(`Get Article by id Route:${articleId}`)
+    const articles = [
+        { id: 1, title: 'First Article', content: 'This is the content of the first article.' },
+        { id: 2, title: 'Second Article', content: 'This is the content of the second article.' },
+        { id: 3, title: 'Third Article', content: 'This is the content of the third article.' },
+        { id: 4, title: 'Fourth Article', content: 'This is the content of the fourth article.' },
+        { id: 5, title: 'Fifth Article', content: 'This is the content of the fifth article.' }
+    ];
+
+    const articleId = parseInt(req.params.articleId); 
+    const article = articles.find(a => a.id === articleId);
+
+    if (article) {
+        res.render('article', { article });
+    } else {
+        res.status(404).send('Article not found');
+    }
 }
 
 export const putArticleByIdHandler = (req, res) => {
